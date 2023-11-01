b3f7839b988c6669f135a06ce5839d147fdd3a14
Hadassah Uche <uhadassah1@gmail.com>
2023-11-01 12:26:46 +0200

    Skeleton done!!!^_^

---
+++ UcheHadassah2_1/HadassahUche2_1.cpp
@@ -0,0 +1,21 @@
+/*Write a function called factorial that takes a number n as a parameter and returns the 
+factorial of that number. In mathematics, the factorial of a number n is defined as:
+n! = n * ( n – 1 ) * ( n – 2 ) * … * 1 and 
+0!=1
+For example in your program, factorial(4) should return 24 (the value of 1*2*3*4), 
+factorial(3) returns 6 (1*2*3). Write a program to test your function with user defined 
+input and user friendly comments*/
+#include<iostream>
+using namespace std;
+int factorial(int n);//Function prototype
+int main()
+{
+
+	return 0;
+}
+int factorial(int n)
+{
+
+	return factorial(n);
+}
+
